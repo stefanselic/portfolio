833434dbcc24349fd368754f41dc25b4217cda0d
stefanselic <s.stefan22@hotmail.com>
2024-09-06 15:52:10 +0200

    Add typing effect for name

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/** @format */
+
+function runTypingEffect() {
+  const text = 'I am Stefan Selic.';
+  const typingElement = document.getElementById('typing-text');
+  const typingDelay = 100;
+
+  typeText(text, typingElement, typingDelay);
+}
+
+function typeText(text, typingElement, delay) {
+  for (let i = 0; i < text.length; i++) {
+    setTimeout(() => {
+      typingElement.textContent += text.charAt(i);
+    }, delay * i);
+  }
+}
+document.addEventListener('DOMContentLoaded', runTypingEffect);
